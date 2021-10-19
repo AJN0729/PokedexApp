5635749ec6e9352bea65d639af5ce012a084a23d
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-10-19 08:32:53 -0500

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by AMStudent on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
