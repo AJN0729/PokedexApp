6ed6a9f73619ab12f775cf0afc116f1f626ece21
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-10-19 09:13:39 -0500

    added radius fade effect to background color.

--- PokedexApp/ContentView.swift
+++ PokedexApp/ContentView.swift
@@ -15,12 +15,24 @@ struct ContentView: View {
                     .font(.title2)
                     .fontWeight(.bold)
                     .foregroundColor(.white)
+                    .padding(.top, 10)
+                    .padding(.leading)
                 
                 HStack {
                     Text("Poison")
                         .font(.caption)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 8)
+                    
+                        .overlay(RoundedRectangle(cornerRadius: 12)
+                        .fill(Color.white.opacity(0.25))
+                    
+                    )
+                    
+                        .frame(width: 100, height: 25)
+                    
                     
                     Image("1")
                         .resizable()
@@ -32,6 +44,7 @@ struct ContentView: View {
         }
         .background(Color.green)
         .cornerRadius(12)
+        .shadow(color: .green, radius:  8, x: 0, y: 0)
     }
 }
 
