d5c7b602a66dc386df9d8af61ff07010c6754903
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-10-20 09:58:24 -0500

    stuff working.

--- PokedexApp/Model/PokemonData.swift
+++ PokedexApp/Model/PokemonData.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct PokemonData: Decodable, Identifiable {
+    let id: Int
+    let name: String
+   // let imageURL: String
+    let type: String
+    let weight: Float
+    let height: Float
+    let description: String
+    let attack: Float
+    let defense: Float
+}
+
+let mockData: [PokemonData] = [
+    .init(id: 0, name: "Bulbasaur", type: "poison", weight: 15, height: 80, description: "Bulbasaur is a thing that people know about", attack: 30, defense: 18),
+    .init(id: 1, name: "Karen", type: "angry person", weight: 300, height: 120, description: "Stay away", attack: 90, defense: 3),
+]
